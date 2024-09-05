c42fc28c2e3bc5862399ca7e1f94d03ef56f86ed
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-09-05 08:27:30 -0300

    refactor: Simpliflying tests and recursive breakdown functions

--- src/sigaa/disciplina/auxiliary.rs
+++ src/sigaa/disciplina/auxiliary.rs
@@ -1,6 +1,5 @@
 use core::panic;
 use std::collections::BTreeSet;
-use std::fmt::format;
 use std::ops::Deref;
 
 use super::Disciplina;
@@ -9,7 +8,7 @@ use super::SigaaTime;
 use regex::Regex;
 
 impl Disciplina {
-    pub fn generate_abreviação(nome: String) -> String {
+    pub fn generate_abreviação(nome: &str) -> String {
         let mut abreviação = String::new();
         let palavras_excluidas = vec!["de", "em", "da", "do", "das", "dos"];
 
@@ -34,14 +33,10 @@ impl Disciplina {
             output = format!("{}{}", output, val.turno);
         }
 
-        for elem in self.sigaa_time.iter() {
-            output = format!("{}{}", output, elem.horario);
-        }
-
         output
     }
-    pub fn is_formatted(time: String) -> bool {
-        let regex = Regex::new(r"^(\d{1,3})([MTN])(\d{2,4})$").unwrap();
+    pub fn is_formatted(time: &str) -> bool {
+        let regex = Regex::new(r"^(\d{1,5})([MTN])(\d{2,6})$").unwrap();
 
         if let Some(captura) = regex.captures(&time) {
             let dd = &captura[1];
@@ -68,53 +63,46 @@ impl Disciplina {
         false
     }
 
-    pub fn valid_string_to_vec_sigaa_times(valid_string: String) -> BTreeSet<SigaaTime> {
+    pub fn valid_string_to_vec_sigaa_times(valid_string: &str) -> BTreeSet<SigaaTime> {
         Self::breakdown(valid_string, &mut BTreeSet::new())
     }
 
-    fn breakdown(value: String, vec: &mut BTreeSet<SigaaTime>) -> BTreeSet<SigaaTime> {
-        let days = Self::extract_day(&value);
-        match value.len() {
-            4 => {
-                vec.insert(value.as_str().try_into().unwrap());
+    fn breakdown(value: &str, vec: &mut BTreeSet<SigaaTime>) -> BTreeSet<SigaaTime> {
+        let days = Self::extract_day(&value).len();
+
+        match days {
+            1 if value.len() == 4 => {
+                vec.insert(value.try_into().unwrap());
             }
-            5 => Self::day_breaker(1, &value, vec),
-            6 if days.len() == 3 => Self::day_breaker(2, &value, vec),
-            6 if days.len() == 1 => Self::hour_breaker(&value, vec),
-            7 => Self::day_breaker(1, &value, vec),
-            8 => Self::day_breaker(2, &value, vec),
-            _ => panic!("{:?}", DisciplinaErrors::WrongInputFormat),
+            1 => Self::hour_breaker(&value, vec),
+            _ => Self::day_breaker(days, &value, vec),
         }
 
         vec.deref().clone()
     }
 
-    fn day_breaker(loops: usize, value: &String, vec: &mut BTreeSet<SigaaTime>) {
-        for i in 0..=loops {
+    fn day_breaker(loops: usize, value: &str, vec: &mut BTreeSet<SigaaTime>) {
+        for i in 0..loops {
             let break_value: String = value
                 .chars()
                 .enumerate()
                 .filter(|(index, _)| *index != i)
                 .map(|(_, c)| c)
                 .collect();
-            Self::breakdown(break_value, vec);
+            Self::breakdown(&break_value, vec);
         }
     }
 
-    fn hour_breaker(value: &String, vec: &mut BTreeSet<SigaaTime>) {
-        if value.len() != 6 {
-            panic!("{:?}", DisciplinaErrors::WrongInputFormat);
-        }
-
+    fn hour_breaker(value: &str, vec: &mut BTreeSet<SigaaTime>) {
         let prefix = &value[..2];
         let first_suffix = &value[2..4];
-        let last_suffix = &value[4..6];
+        let last_suffix = &value[4..];
 
         let first_part = format!("{}{}", prefix, first_suffix);
         let last_part = format!("{}{}", prefix, last_suffix);
 
-        Self::breakdown(first_part, vec);
-        Self::breakdown(last_part, vec);
+        Self::breakdown(&first_part, vec);
+        Self::breakdown(&last_part, vec);
     }
 
     fn extract_day(texto: &str) -> &str {
@@ -122,7 +110,7 @@ impl Disciplina {
 
         match finder {
             Some(pos) => texto.split_at(pos).0,
-            None => panic!("{:?}", DisciplinaErrors::WrongInputFormat),
+            None => panic!("{:?}: {}", DisciplinaErrors::TurnoNotFounded, texto),
         }
     }
 }
@@ -136,56 +124,77 @@ mod tests {
 
     #[test]
     fn should_generate_a_correct_abrv() {
-        let s: String = "Algo Muideto Foda de tlgd".into();
-        let abv = Disciplina::generate_abreviação(s);
+        let abv = Disciplina::generate_abreviação("Algo Muideto Foda de tlgd");
         assert_eq!(abv, "AMFT")
     }
 
     #[test]
     fn should_return_true_with_formatted_strings() {
-        assert!(Disciplina::is_formatted("246T56".to_string()));
-        assert!(Disciplina::is_formatted("2T12".to_string()));
-        assert!(Disciplina::is_formatted("5N34".to_string()));
-        assert!(Disciplina::is_formatted("36M12".to_string()));
-        assert!(Disciplina::is_formatted("246M56".to_string()));
+        assert!(Disciplina::is_formatted("246T56"));
+        assert!(Disciplina::is_formatted("2T12"));
+        assert!(Disciplina::is_formatted("5N34"));
+        assert!(Disciplina::is_formatted("36M12"));
+        assert!(Disciplina::is_formatted("246M56"));
     }
 
     #[test]
     fn should_return_false_with_non_formatted_strings() {
-        assert!(!Disciplina::is_formatted("24X34".to_string()));
-        assert!(!Disciplina::is_formatted("29M12".to_string()));
-        assert!(!Disciplina::is_formatted("1234T1234".to_string()));
+        assert!(!Disciplina::is_formatted("24X34"));
+        assert!(!Disciplina::is_formatted("29M12"));
+        assert!(!Disciplina::is_formatted("1234T1234"));
     }
 
     #[test]
-    fn should_create_a_correct_list_of_sigaa_times() {
-        let time = "246T12".to_string();
-        let sigaa_times = Disciplina::valid_string_to_vec_sigaa_times(time);
-        let sigaa_time_1 = SigaaTime::new_from_strings("2", "T", "12").unwrap();
-        let sigaa_time_2 = SigaaTime::new_from_strings("4", "T", "12").unwrap();
-        let sigaa_time_3 = SigaaTime::new_from_strings("6", "T", "12").unwrap();
+    fn should_create_a_correct_list_of_sigaa_time_days() {
+        let sigaa_times = Disciplina::valid_string_to_vec_sigaa_times("246T12");
+
+        let sigaa_time_1 = SigaaTime::new_from_strings("2", "T12").unwrap();
+        let sigaa_time_2 = SigaaTime::new_from_strings("4", "T12").unwrap();
+        let sigaa_time_3 = SigaaTime::new_from_strings("6", "T12").unwrap();
 
         let mut sigaa_times_iter = sigaa_times.iter();
 
-        if let Some(val) = sigaa_times_iter.next() {
-            assert!(*val == sigaa_time_1);
-        }
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_1));
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_2));
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_3));
+    }
 
-        if let Some(val) = sigaa_times_iter.next() {
-            assert_eq!(*val, sigaa_time_2);
-        }
+    #[test]
+    fn should_create_a_correct_list_of_sigaa_time_hour() {
+        let sigaa_times = Disciplina::valid_string_to_vec_sigaa_times("2T123456");
+        let sigaa_time_1 = SigaaTime::new_from_strings("2", "T12").unwrap();
+        let sigaa_time_2 = SigaaTime::new_from_strings("2", "T34").unwrap();
+        let sigaa_time_3 = SigaaTime::new_from_strings("2", "T56").unwrap();
 
-        if let Some(val) = sigaa_times_iter.next() {
-            assert_eq!(*val, sigaa_time_3);
-        }
+        let mut sigaa_times_iter = sigaa_times.iter();
+
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_1));
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_2));
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_3));
+    }
+
+    #[test]
+    fn should_create_a_correct_list_of_sigaa_time_days_and_hours() {
+        let sigaa_times = Disciplina::valid_string_to_vec_sigaa_times("246T1234");
+        let sigaa_time_1 = SigaaTime::new_from_strings("2", "T12").unwrap();
+        let sigaa_time_2 = SigaaTime::new_from_strings("2", "T34").unwrap();
+        let sigaa_time_3 = SigaaTime::new_from_strings("4", "T12").unwrap();
+        let sigaa_time_4 = SigaaTime::new_from_strings("4", "T34").unwrap();
+        let sigaa_time_5 = SigaaTime::new_from_strings("6", "T12").unwrap();
+        let sigaa_time_6 = SigaaTime::new_from_strings("6", "T34").unwrap();
+        let mut sigaa_times_iter = sigaa_times.iter();
+
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_1));
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_2));
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_3));
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_4));
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_5));
+        assert_eq!(sigaa_times_iter.next(), Some(&sigaa_time_6));
     }
 
     #[test]
     fn should_generate_a_correct_sigaa_time_display() {
-        let disciplina =
-            Disciplina::new_with_time_string("fun mat comp".to_string(), "246T12".to_string())
-                .unwrap();
-
+        let disciplina = Disciplina::new_stringify("fun mat comp", "246T12").unwrap();
         assert_eq!(disciplina.generate_horario_display(), "246T12");
     }
 }

a933bc0780dd6bca6d5c6c84ee21020ea6b2011d
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-09-05 08:28:09 -0300

    refactor: Using &str instead of String

--- src/sigaa/disciplina/implementation.rs
+++ src/sigaa/disciplina/implementation.rs
@@ -5,36 +5,25 @@ use super::SigaaTime;
 use super::{Disciplina, DisciplinaErrors};
 
 impl Disciplina {
-    pub fn new(nome: String) -> Disciplina {
-        Disciplina {
-            sigaa_time: BTreeSet::new(),
-            abreviacao: Self::generate_abreviação(nome.clone()),
-            nome,
-        }
-    }
-
-    pub fn new_with_time(nome: String, sigaa_time: BTreeSet<SigaaTime>) -> Disciplina {
+    pub fn new(nome: &str, sigaa_time: BTreeSet<SigaaTime>) -> Disciplina {
         Disciplina {
             sigaa_time,
-            abreviacao: Self::generate_abreviação(nome.clone()),
-            nome,
+            abreviacao: Self::generate_abreviação(nome),
+            nome: nome.to_string(),
         }
     }
 
-    pub fn new_with_time_string(
-        nome: String,
-        new_time: String,
-    ) -> Result<Disciplina, DisciplinaErrors> {
-        if !Self::is_formatted(new_time.clone()) {
-            return Err(DisciplinaErrors::WrongInputFormat);
+    pub fn new_stringify(nome: &str, new_time: &str) -> Result<Disciplina, DisciplinaErrors> {
+        if !Self::is_formatted(new_time) {
+            return Err(DisciplinaErrors::NotFormatted);
         }
 
         let new_times: BTreeSet<SigaaTime> = Self::valid_string_to_vec_sigaa_times(new_time);
 
         Ok(Disciplina {
             sigaa_time: new_times,
-            abreviacao: Self::generate_abreviação(nome.clone()),
-            nome,
+            abreviacao: Self::generate_abreviação(nome),
+            nome: nome.to_string(),
         })
     }
 
